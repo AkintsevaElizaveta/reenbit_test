d6ba559a9eb23599c9777c32921bae791a8106a4
Yelyzaveta Akintseva <akincevae@gmail.com>
2022-08-24 19:24:06 +0300

    added SELECTED for chats

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, {useEffect} from "react";
 import Context from "./context/Context";
 import ContactApi from "./api/contacts/ContactApi";
 import ContactList from "./components/contacts/ContactList";
+import Chat from "./components/chats/Chat";
+import chat from "./components/chats/Chat";
 
 function App() {
 
     const [contacts, setContacts] = React.useState([]);
+    const [chats, setChats] = React.useState('123')
 
     useEffect(() => {
         ContactApi.getContacts()
             .then(items => setContacts(items));
 
     }, [])
+
+    function selectChat(usersId){
+        setChats(usersId)
+    }
+
+    console.log(`chat: ${chats}`)
 
     return (
         <Context.Provider>
@@ -21,9 +30,11 @@ function App() {
                         <input className="contacts__search_field"/>
                     </div>
                     <h3 className="contacts__title">Chats</h3>
-                    <ContactList contacts={contacts}></ContactList>
+                    <ContactList contacts={contacts} onSelect={selectChat}></ContactList>
                 </div>
-                <div>Chat</div>
+                <div>
+                    <Chat usersId={chats}></Chat>
+                </div>
             </div>
 
         </Context.Provider>
